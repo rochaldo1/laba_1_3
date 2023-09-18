b879f5abd4f10983983ace2b03178fffe25b9255
Dmitry <dima2323fr3@gmail.com>
2023-09-19 00:38:39 +0700

    third

--- LAB_1_PER_3/Flower.cs
+++ LAB_1_PER_3/Flower.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LAB_1_PER_3
 {
-    internal class Class1
+    public enum Color
     {
+        Red, Green, Blue
+    };
+
+    internal class Flower
+    {
+        string name = "Flower";
+        Color color = Color.Red;
+        double state = 1, fadingInWater = 1, fadingInAir = 1, basePrice = 0;
+        bool inWater = false;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
     }
 }
--- LAB_1_PER_3/Program.cs
+++ LAB_1_PER_3/Program.cs
@@ -4,12 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace LAB_1_PER_3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Flower a=new Flower();
+            Console.WriteLine(a.Name);
         }
     }
 }
